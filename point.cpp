ff3b6d8ca811bb6e692772836c74142caa9ae234
ghazal <ghazalhasanpour@gmail.com~>
2018-02-11 23:53:15 +0330

    point added to project

---
+++ point.cpp
@@ -0,0 +1,11 @@
+
+#include "point.h"
+Point::Point(int a, int b) {
+    x=a;
+    y=b;
+}
+Point::Point() {x=0;y=0;}
+Suggestion::Suggestion() {flips=0;}
+Suggestion::Suggestion(int a, int b) {x=a;y=b;flips=0;}
+//
+
